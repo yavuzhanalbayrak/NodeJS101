db8cb1eb4165d1bd09fed45010cf74ad6fd0499d
Yavuzhan Albayrak <82886846+yavuzhanalbayrak@users.noreply.github.com>
2024-02-14 18:04:31 +0300

    socket.io

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { AddEdit } from './pages/addedit/AddEdit';
 import { Home } from './pages/home/Home';
 import { Routes, Route } from 'react-router-dom';
 import {BrowserRouter as Router} from "react-router-dom";
+import { Chat } from './pages/chat/Chat';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/add" element={<AddEdit/>} />
           <Route path="/update/:id" element={<AddEdit/>} />
+          <Route path="/chat" element={<Chat/>} />
 
           </Routes>
       </div>
