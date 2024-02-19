89ba9e5c75e07fa36d1820ff4e4f7b8b3dc5abe0
Yavuzhan Albayrak <82886846+yavuzhanalbayrak@users.noreply.github.com>
2024-02-19 10:06:56 +0300

    loggerMiddleware fonksiyon ataması düzeltildi.

--- backend/middlewares/loggerMiddleware.js
+++ backend/middlewares/loggerMiddleware.js
@@ -1,5 +1,5 @@
 const loggerMiddleware = (app)=>{
-    app.use(function(req, res, next) {
+    app.use((req, res, next) => {
         console.log('Bir istek alındı:', req.method, req.url);
         next(); // Sonraki middleware'e devret
     });
